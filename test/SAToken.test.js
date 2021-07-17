22325de80181c5eed9f3a92fd09c5ddc9284067f
Francesco Sullo <sullof@gmail.com>
2021-07-17 13:16:18 -0700

    Remove skip in tests

--- test/SAToken.test.js
+++ test/SAToken.test.js
@@ -1,7 +1,7 @@
 const {expect, assert} = require("chai")
 const {assertThrowsMessage} = require('./helpers')
 
-describe.only("SAToken", async function () {
+describe("SAToken", async function () {
 
   let SAStorage
   let storage
