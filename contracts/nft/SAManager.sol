d333f6b75117c889e65e747a8bcb82d4dc7d304f
Francesco Sullo <sullof@gmail.com>
2021-07-23 10:58:28 -0700

    Optimize ISATokenMin

--- contracts/nft/SAManager.sol
+++ contracts/nft/SAManager.sol
@@ -12,24 +12,6 @@ import "hardhat/console.sol";
 
 interface ISATokenMin {
 
-  function pause(uint tokenId) external;
-
-  function unpause(uint tokenId) external;
-
-  function pauseBatch(uint[] memory tokenIds) external;
-
-  function unpauseBatch(uint[] memory tokenIds) external;
-
-  function isPaused(uint tokenId) external view returns (bool);
-
-  function updateFactory(address factoryAddress) external;
-
-  function updateStorage(address storageAddress) external;
-
-  function factory() external view returns (address);
-
-  function mint(address to, uint256 amount) external;
-
   function mintWithExistingBundle(address to) external;
 
   function nextTokenId() external view returns (uint);
