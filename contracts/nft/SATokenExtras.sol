b52ebaba56a5c1fc19b660d8ea0cada8cced64f0
Francesco Sullo <sullof@gmail.com>
2021-07-31 16:19:13 -0700

    Removing ISATokenExtended.sol

--- contracts/nft/SATokenExtras.sol
+++ contracts/nft/SATokenExtras.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-import "./ISATokenExtended.sol";
 import "./ISATokenExtras.sol";
 import "./ISAStorage.sol";
 import "../sale/ISale.sol";
@@ -12,6 +11,29 @@ import "../utils/IERC20Optimized.sol";
 import "../user/IProfile.sol";
 
 import "hardhat/console.sol";
+
+// this is an extended version of the interface, including ISAStorage functions
+interface ISAToken {
+
+  function updateFactory(address factoryAddress) external;
+
+  function mint(address to, address sale, uint256 amount, uint128 vestedPercentage) external;
+
+  function nextTokenId() external view returns (uint256);
+
+  function burn(uint256 tokenId) external;
+
+  function vest(uint256 tokenId) external returns (bool);
+
+  function ownerOf(uint256 tokenId) external view returns (address);
+
+  function getBundle(uint256 bundleId) external view returns (ISAStorage.Bundle memory);
+
+  function increaseAmountInSA(uint256 bundleId, uint256 i, uint256 diff) external;
+
+  function addSAToBundle(uint256 bundleId, ISAStorage.SA memory newSA) external;
+
+}
 
 contract SATokenExtras is ISATokenExtras, LevelAccess {
 
