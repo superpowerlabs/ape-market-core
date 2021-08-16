8ec23f2660894e3343227b2c3571776ecdbf4f43
zhimin <zhiminhe@gmail.com>
2021-08-15 21:48:43 -0700

    apply registry to Sale

--- contracts/sale/Sale.sol
+++ contracts/sale/Sale.sol
@@ -11,6 +11,7 @@ contract Sale is RegistryUser {
   using SafeMath for uint256;
 
   uint16 private _saleId;
+  ISaleData _saleData;
 
   modifier onlySaleOwner() {
     require(_msgSender() == ISaleData(_get("SaleData")).getSetupById(_saleId).owner, "Sale: caller is not the owner");
@@ -35,23 +36,21 @@ contract Sale is RegistryUser {
   // Invest amount into the sale.
   // Investor needs to approve the payment + fee amount need for purchase before calling this
   function invest(uint32 amount) external virtual {
-    ISaleData saleData = ISaleData(_get("SaleData"));
-    ISaleDB.Setup memory setup = saleData.getSetupById(_saleId);
-    (uint256 tokenPayment, uint256 buyerFee) = saleData.setInvest(_saleId, _msgSender(), amount);
-    IERC20Min paymentToken = IERC20Min(saleData.paymentTokenById(setup.paymentTokenId));
-    paymentToken.transferFrom(_msgSender(), saleData.apeWallet(), buyerFee);
+
+    ISaleDB.Setup memory setup = _saleData.getSetupById(_saleId);
+    (uint256 tokenPayment, uint256 buyerFee) = _saleData.setInvest(_saleId, _msgSender(), amount);
+    IERC20Min paymentToken = IERC20Min(_saleData.paymentTokenById(setup.paymentTokenId));
+    paymentToken.transferFrom(_msgSender(), _saleData.apeWallet(), buyerFee);
     paymentToken.transferFrom(_msgSender(), address(this), tokenPayment);
   }
 
   function withdrawPayment(uint256 amount) external virtual onlySaleOwner {
-    ISaleData saleData = ISaleData(_get("SaleData"));
-    IERC20Min paymentToken = IERC20Min(saleData.paymentTokenById(saleData.getSetupById(_saleId).paymentTokenId));
+    IERC20Min paymentToken = IERC20Min(_saleData.paymentTokenById(_saleData.getSetupById(_saleId).paymentTokenId));
     paymentToken.transfer(_msgSender(), amount);
   }
 
   function withdrawToken(uint256 amount) external virtual onlySaleOwner {
-    ISaleData saleData = ISaleData(_get("SaleData"));
-    (IERC20Min sellingToken, uint256 fee) = saleData.setWithdrawToken(_saleId, amount);
+    (IERC20Min sellingToken, uint256 fee) = _saleData.setWithdrawToken(_saleId, amount);
     sellingToken.transfer(_msgSender(), amount + fee);
   }
 
@@ -61,14 +60,18 @@ contract Sale is RegistryUser {
     uint120 remainingAmount,
     uint256 requestedAmount
   ) external virtual onlyFrom("SANFTManager") returns (bool) {
-    ISaleData saleData = ISaleData(_get("SaleData"));
-    if (saleData.isVested(_saleId, fullAmount, remainingAmount, requestedAmount)) {
-      saleData.getSetupById(_saleId).sellingToken.transfer(saOwner, requestedAmount);
+    if (_saleData.isVested(_saleId, fullAmount, remainingAmount, requestedAmount)) {
+      _saleData.getSetupById(_saleId).sellingToken.transfer(saOwner, requestedAmount);
       return true;
     } else {
       return false;
     }
   }
 
-  function updateRegisteredContracts() external virtual override {}
+  function updateRegisteredContracts() external virtual override onlyRegistry {
+    address addr = _get("SaleData");
+    if (addr != address(_saleData)) {
+      _saleData = ISaleData(addr);
+    }
+  }
 }
