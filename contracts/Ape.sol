fe7a4e8a54fb4277369e483afb9d36b84308b18e
zhimin <zhiminhe@gmail.com>
2021-09-10 11:03:35 -0700

    Ape token for minting using remix

--- contracts/Ape.sol
+++ contracts/Ape.sol
@@ -4,9 +4,6 @@ pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/token/ERC20/ERC20.sol";
 contract Ape is ERC20 {
   constructor(string memory tokenName, string memory tokenSymbol, address receiver) ERC20(tokenName, tokenSymbol) {
-    if (receiver == address(0)) {
-        receiver = msg.sender;
-    }
     _mint(receiver, 10 ** 27);  // 1B token, with default 18 decimals
   }
 }
